58fa0f34891f401a7d984a47e46a54fe0a8d1b02
amir-razmi <amir.dv8212@gmail.com>
2025-09-02 18:13:47 +0330

    fix exporting types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "./services/Solana.utils";
 export * from "./constants/sol.constant";
-export * from "./types/solanaTransaction.type";
-export * from "./types/token.type";
+export type * from "./types/solanaTransaction.type";
+export type * from "./types/token.type";
